d8731fbe5c9c38d5bbc64be37c5918626fef32c3
Andrei Durlea <andrei.durlea.telefon@gmail.com>
2026-04-02 02:52:39 +0300

    ci: add sample function to fix ci

--- include/beman/at_most/at_most.hpp
+++ include/beman/at_most/at_most.hpp
@@ -3,4 +3,13 @@
 #ifndef BEMAN_AT_MOST_AT_MOST_HPP
 #define BEMAN_AT_MOST_AT_MOST_HPP
 
+namespace beman::at_most {
+/**
+ * Returns the version of the beman.at_most library.
+ * @return The version number.
+ */
+constexpr int version() noexcept { return 1; }
+
+} // namespace beman::at_most
+
 #endif // BEMAN_AT_MOST_AT_MOST_HPP
--- tests/beman/at_most/at_most.test.cpp
+++ tests/beman/at_most/at_most.test.cpp
@@ -3,4 +3,6 @@
 #include <beman/at_most/at_most.hpp>
 #include <gtest/gtest.h>
 
+TEST(AtMostTest, VersionTest) { EXPECT_EQ(beman::at_most::version(), 1); }
+
 TEST(AtMostTest, BlankTest) { EXPECT_TRUE(true); }
