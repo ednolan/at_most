0856bd0426c40b2a2f3a2456828e57aea0dff8ac
Andrei Durlea <andrei.durlea.telefon@gmail.com>
2026-04-04 14:42:23 +0300

    feat: implement partial_sort_at_most for ranges
    as a Niebloid object (wow! those things are cool)

--- include/beman/at_most/partial_sort_at_most.hpp
+++ include/beman/at_most/partial_sort_at_most.hpp
@@ -23,6 +23,36 @@ constexpr void partial_sort_at_most(RandomAccessIterator                        
     std::ranges::partial_sort(first, mid, last, comp);
 }
 
+namespace ranges {
+
+namespace __partial_sort_at_most {
+
+struct __fn {
+    template <std::random_access_iterator I,
+              std::sentinel_for<I>        S,
+              typename Comp = std::ranges::less,
+              typename Proj = std::identity>
+        requires std::sortable<I, Comp, Proj>
+    constexpr I operator()(I first, S last, std::iter_difference_t<I> n, Comp comp = {}, Proj proj = {}) const {
+        auto k   = std::max(std::iter_difference_t<I>(0), n);
+        auto mid = std::ranges::next(first, k, last);
+        return std::ranges::partial_sort(first, mid, last, comp, proj);
+    }
+
+    template <std::ranges::random_access_range R, typename Comp = std::ranges::less, typename Proj = std::identity>
+        requires std::sortable<std::ranges::iterator_t<R>, Comp, Proj>
+    constexpr std::ranges::borrowed_iterator_t<R>
+    operator()(R&& r, std::ranges::range_difference_t<R> n, Comp comp = {}, Proj proj = {}) const {
+        return operator()(std::ranges::begin(r), std::ranges::end(r), n, comp, proj);
+    }
+};
+
+} // namespace __partial_sort_at_most
+
+inline constexpr __partial_sort_at_most::__fn partial_sort_at_most{};
+
+} // namespace ranges
+
 } // namespace beman::at_most
 
 #endif // BEMAN_AT_MOST_PARTIAL_SORT_AT_MOST_HPP
